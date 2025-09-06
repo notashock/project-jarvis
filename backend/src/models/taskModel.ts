06fb991ed632d25e19362ebfd19080cc58d73611
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-09-07 03:09:47 +0530

    Delete backend directory

--- backend/src/models/taskModel.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const taskSchema = new Schema({
-  title: { type: String, required: true },
-  deadline: { type: Date, required: true },
-  done: { type: Boolean, default: false }
-}, { timestamps: true });
-
-export default model("Task", taskSchema);
