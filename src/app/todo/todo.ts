a808164acf78926eb83c818511695e978d339e21
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-09-07 22:39:03 +0530

    feat: implement Todo component with HTML, CSS, and TypeScript files

---
+++ src/app/todo/todo.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo.html',
+  styleUrl: './todo.css'
+})
+export class TodoComponent {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
