d2067921ccf230a3a7b317d8fd1f19e555698977
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-09-22 12:30:03 +0530

    feat: add new routes for better navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeDashboardComponent } from './pages/home-dashboard/home-dashboard';
+import { EmailsComponent } from './pages/emails/emails';
+import { EmailDetailsComponent } from './pages/email-details/email-details';
+import { ClassroomComponent } from './pages/classroom/classroom';
+import { ClassDetailsComponent } from './pages/class-details/class-details';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // default route
   { path: 'dashboard', component: HomeDashboardComponent },
+  { path: 'emails', component: EmailsComponent },
+  { path: 'emails/:id', component: EmailDetailsComponent },
+  { path: 'classroom', component: ClassroomComponent },
+  { path: 'classroom/:id', component: ClassDetailsComponent },
 ];
