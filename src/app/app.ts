5444f91b8aa970165e114c069a76766a100d618d
Ashok Babu Bavireddy <130152937+notashock@users.noreply.github.com>
2025-09-22 12:29:00 +0530

    refactor: redesigned for multi page layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class AppComponent {
   title = 'Jarvis';
